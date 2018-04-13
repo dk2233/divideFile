1717da34a434b60c68626a33df8c99b4a5e7345e
dk2233 <kucharski22daniel@gmail.com>
2018-04-13 23:31:46 +0200

    all was correct, it was simple problem of not removing "\n" at the end of line that was read

--- main.c
+++ main.c
@@ -1,31 +1,67 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
+
+#include <unistd.h>
 
 #define SEARCH_FILE_NAME   "<file"
 #define END_OF_FILE        "</file"
+#define BUFFER_LENGTH 		150U
+
+#if defined(__WIN32) ||  defined(WIN32)
+#define		HOW_MANY_DIGIT_LESS		1U
+#define	HOW_MANY_DIGIT_LESS2  0U
+#elif defined(__APPLE__)
+#define		HOW_MANY_DIGIT_LESS		1U
+#endif
+
 
 void removeAllsymbol(char symbol, char * array_c);
 void copyAllLineToFile(char *file_name, FILE *fromFile);
+void showAllChars(char *string);
+
+
+void showAllChars(char *string)
+{
+	int i;
+	for(i=0; i<strlen(string); i++)
+	{
+		printf("%d %c \n",i,string[i]);
+	}
+}
 
 
 void copyAllLineToFile(char *file_name, FILE *fromFile)
 {
 	FILE *file1;
-	printf(" file to open %s",file_name);
-	file1= fopen(file_name,"w");
+
+	char *line;
+	//char *fileName2;
+	char tab[BUFFER_LENGTH]={0};
+
+	strcpy(tab,".\\");
+	strcat(tab,file_name);
+	printf(" file to open %s",tab);
+	file1= fopen(tab,"w+");
+	printf("%ld",(long)file1);
+	line= malloc(BUFFER_LENGTH);
+
+	if (NULL == file1)
+	{
+		perror(" there is a problem opening file :");
+		exit(1);
+	}
 	printf("\n open file %s \n",file_name);
-	char *line = malloc(300);
-	fgets(line,300,fromFile);
+	fgets(line,BUFFER_LENGTH,fromFile);
 
 	while( (line!=NULL) &&  (strstr(line,END_OF_FILE) == NULL)  )
 	{
 
 
 		fprintf(file1,"%s",line);
-		//printf("%s",line);
-		fgets(line,300,fromFile);
+		/*printf("%s",line); */
+		fgets(line,BUFFER_LENGTH,fromFile);
 
 	}
 
@@ -42,53 +78,57 @@ void copyAllLineToFile(char *file_name, FILE *fromFile)
 void removeAllsymbol(char symbol, char * array_c)
 {
 	int i, j;
-	//char *word1 = malloc(strlen(array_c));
-	char *word2 = malloc(strlen(array_c));
 
+	printf(" length of string %d \n",strlen(array_c));
 	while( strchr(array_c, symbol) )
 	{
 		for(i=0; i<strlen(array_c);i++)
 		{
 			if  (array_c[i] == symbol)
 			{
-				printf(" %d ",i);
-				for(j=i; j<(strlen(array_c)-1);j++)
+				printf("position %d of %c \n",i,array_c[i]);
+				for(j=i; j<(strlen(array_c));j++)
 				{
 					array_c[j]=array_c[j+1];
 					printf("%d -> %c ",j,array_c[j]);
 				}
-				//array_c[j+1]='\0';
-				array_c[j-1]='\0';
-				printf(" removed %c in %s \n\n",symbol,word2);
-				return;
+				printf(" removed %c in %s \n\n",symbol,array_c);
+
 			}
 		}
+
 	}
 
-
+	printf("AFTER length of string %d \n",strlen(array_c));
 
 }
 
 
 int main(int argc, char *argv[])
 {
-	char *line = malloc(300);
-	char *temp = malloc(300);
-	//uint8_t nr_line;
-
+	char *line = malloc(BUFFER_LENGTH);
+	char *temp = malloc(BUFFER_LENGTH);
 	FILE *file_to_analyse;
+
+
+	printf(" \n\n *************** \n\n");
+	//line = "abc";
+	//showAllChars(line);
 	if (argc>1)
 	{
 		printf(" I opened file %s ",argv[1]);
 
 		file_to_analyse = fopen(argv[1],"r");
-
-
-		fgets(line,300,file_to_analyse);
-		while(*line != (char)NULL)
+		/*file_to_analyse = fopen("RS_all_set.ch","r");*/
+		if (NULL == file_to_analyse)
+		{
+			perror(" I cant open file:");
+			exit(0);
+		}
+		fgets(line,BUFFER_LENGTH,file_to_analyse);
+		while(line != NULL)
 		{
 
-			printf("line  : %s \n",line);
 
 			if (strstr(line,SEARCH_FILE_NAME) != NULL)
 			{
@@ -98,10 +138,14 @@ int main(int argc, char *argv[])
 				if (temp != NULL)
 				{
 
-					//char *loc_p = strchr(line,">");
+					showAllChars(temp);
+					removeAllsymbol('\n',temp);
 					removeAllsymbol('>',temp);
-					printf("after1: %s \n",temp);
 					removeAllsymbol(' ',temp);
+					showAllChars(temp);
+					/*char *name = malloc(strlen(temp));
+					strcpy(name,temp);*/
+
 					copyAllLineToFile(temp,file_to_analyse);
 				}
 				else
@@ -112,12 +156,17 @@ int main(int argc, char *argv[])
 
 			}
 
-			*line = (char)NULL;
-			fgets(line,300,file_to_analyse);
+			//line = NULL;
+			fgets(line,BUFFER_LENGTH,file_to_analyse);
 
 
 		}
-		fclose(file_to_analyse);
+	fclose(file_to_analyse);
 	}
+	else
+	{
+		printf(" \n Please give file name as an arguement \n");
+	}
+
 	return 0;
 }
